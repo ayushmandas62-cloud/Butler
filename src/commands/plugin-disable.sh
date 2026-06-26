5e225240f291dff90057bec360d32034c4e21f7b
Ayushman Das <ayushmandas62@gmail.com>
2026-06-26 13:33:44 +0530

    Add plugin disable command

---
+++ src/commands/plugin-disable.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+source "$SCRIPT_DIR/../lib/common.sh"
+source "$SCRIPT_DIR/../lib/logger.sh"
+
+PROJECT_ROOT="$(realpath "$SCRIPT_DIR/../..")"
+PLUGIN_DIR="$PROJECT_ROOT/plugins"
+
+if [ $# -ne 1 ]; then
+    die "Usage: butler plugin-disable <plugin>"
+fi
+
+PLUGIN="$1"
+
+if [ ! -f "$PLUGIN_DIR/${PLUGIN}.sh" ]; then
+    die "Plugin '$PLUGIN' not installed."
+fi
+
+mv "$PLUGIN_DIR/${PLUGIN}.sh" "$PLUGIN_DIR/${PLUGIN}.disabled"
+
+success "Plugin '$PLUGIN' disabled."
